70048df46b2ac9b03d85122c4dce6bdf15ff8add
zhuren-plbm <zhuren@phonglambaomat.online>
2025-11-05 21:07:03 +0700

    chore(spider): add coinspect.com

---
+++ tulsa/spiders/blog/coinspect_com.py
@@ -0,0 +1,45 @@
+import json
+from datetime import datetime
+from time import mktime
+from typing import override
+
+from crawlee.crawlers import ParselCrawlingContext
+from crawlee.statistics import FinalStatistics
+
+from tulsa import HtmlSpider
+from tulsa.helpers import parse_date
+from tulsa.models import Blog, Category
+
+
+async def default_request_handler(context: ParselCrawlingContext):
+    data = context.selector.xpath(
+        '//head/script[@type="application/ld+json"]/text()'
+    ).get()
+    if not data:
+        context.log.error(
+            f"{context.request.url} | Cannot find <script type='application/ld+json'> HTML element"
+        )
+        return
+    for entry in json.loads(data).get("hasPart", []):
+        if not entry["@type"] == "Article":
+            continue
+        title = entry["name"]
+        url = entry["url"]
+        description = entry["description"]
+        published = parse_date(entry["datePublished"])
+
+        item = Blog(url=url, title=title, category=Category.Blockchain)
+        item.description = description
+        if published:
+            item.published = datetime.fromtimestamp(mktime(published))
+
+        yield item
+
+
+class CoinspectComSpider(HtmlSpider):
+    def __init__(self) -> None:
+        super().__init__(default_request_handler=default_request_handler)
+
+    @override
+    async def run(self) -> FinalStatistics:  # pyright: ignore [reportIncompatibleMethodOverride]
+        return await super().run(["https://www.coinspect.com/blog"])
