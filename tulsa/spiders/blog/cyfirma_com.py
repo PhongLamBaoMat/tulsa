195b8da72847e350258778e0361573dc71b393a5
zhuren-plbm <zhuren@phonglambaomat.online>
2025-11-15 10:51:44 +0700

    chore(spider): add cyfirma.com

---
+++ tulsa/spiders/blog/cyfirma_com.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from time import mktime
+from typing import override
+from urllib.parse import urljoin
+
+from crawlee.crawlers import ParselCrawlingContext
+from crawlee.statistics import FinalStatistics
+
+from tulsa import Spider
+from tulsa.helpers import parse_date
+from tulsa.models import Blog
+
+
+class CyfirmaComSpider(Spider):
+    def __init__(self) -> None:
+        super().__init__(default_request_handler=self.default_request_handler)
+
+    @override
+    async def run(self) -> FinalStatistics:  # pyright: ignore [reportIncompatibleMethodOverride]
+        return await super().run(
+            [
+                "https://www.cyfirma.com/cyber-research/",
+                "https://www.cyfirma.com/whitepapers/",
+            ]
+        )
+
+    @staticmethod
+    async def default_request_handler(context: ParselCrawlingContext):
+        items = context.selector.xpath(
+            '//div[@class="col-12 col-lg-6 col-md-6 blogs mb-4"]'
+        )
+
+        if len(items) == 0:
+            context.log.error(f"{context.request.url} | Cannot find HTML element")
+            return
+
+        for entry in items:
+            title = entry.xpath(".//h5/text()").get()
+            if not title:
+                context.log.error(
+                    f"{context.request.url} | Cannot find title HTML element"
+                )
+                continue
+            url = entry.xpath("./a/@href").get()
+            if not url:
+                context.log.error(
+                    f"{context.request.url} | Cannot find url HTML element"
+                )
+                continue
+            url = urljoin(context.request.loaded_url or context.request.url, url)
+            description = entry.xpath(".//p/text()").get()
+            thumbnail = entry.xpath(".//img/@src").get()
+            published = entry.xpath(".//date/text()").get()
+
+            item = Blog(url=url, title=title.strip())
+            if description:
+                item.description = description
+            if thumbnail:
+                item.thumbnail = urljoin(
+                    context.request.loaded_url or context.request.url, thumbnail
+                )
+            if published:
+                published = parse_date(published)
+                if published:
+                    item.published = datetime.fromtimestamp(mktime(published))
+
+            yield item
