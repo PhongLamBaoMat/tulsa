2691561f5ed486fddf7dee45b279eb02370bf579
zhuren-plbm <zhuren@phonglambaomat.online>
2025-11-16 23:32:54 +0700

    chore(spider): add onekey.com

---
+++ tulsa/spiders/blog/onekey_com.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from time import mktime
+from typing import override
+from urllib.parse import urljoin
+
+from crawlee.crawlers import ParselCrawlingContext
+from crawlee.statistics import FinalStatistics
+
+from tulsa import Spider
+from tulsa.helpers import parse_date
+from tulsa.models import Blog
+
+
+class OnekeyComSpider(Spider):
+    def __init__(self) -> None:
+        super().__init__(default_request_handler=self.default_request_handler)
+
+    @override
+    async def run(self) -> FinalStatistics:  # pyright: ignore [reportIncompatibleMethodOverride]
+        return await super().run(["https://www.onekey.com/resources/research"])
+
+    @staticmethod
+    async def default_request_handler(context: ParselCrawlingContext):
+        items = context.selector.xpath('//div[@hover="parent" and @role="listitem"]')
+
+        if len(items) == 0:
+            context.log.error(f"{context.request.url} | Cannot find HTML element")
+            return
+
+        for entry in items:
+            title = entry.xpath(".//h3/text()").get()
+            if not title:
+                context.log.error(
+                    f"{context.request.url} | Cannot find title HTML element"
+                )
+                continue
+            url = entry.xpath("./a/@href").get()
+            if not url:
+                context.log.error(
+                    f"{context.request.url} | Cannot find url HTML element"
+                )
+                continue
+            url = urljoin(context.request.loaded_url or context.request.url, url)
+            published = entry.xpath(
+                './/div[@class="featured-rsrc_item_tag text-size-regular text-style-tag text-weight-bold"]/text()'
+            )[1].get()
+            description = entry.xpath(
+                './/p[@class="featured-rsrc_item_paragraph text-size-regular text-color-2"]/text()'
+            ).get()
+            thumbnail = entry.xpath(".//img/@src").get()
+
+            item = Blog(url=url, title=title)
+            if description:
+                item.description = description
+            if thumbnail:
+                item.thumbnail = thumbnail
+            if published:
+                published = parse_date(published)
+                if published:
+                    item.published = datetime.fromtimestamp(mktime(published))
+
+            yield item
