fed5adcb12c1171011f7acbea8a75da519f7fe37
zhuren-plbm <zhuren@phonglambaomat.online>
2025-11-17 15:27:49 +0700

    chore(spider): add trmlabs.com

---
+++ tulsa/spiders/blog/trmlabs_com.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from time import mktime
+from typing import override
+from urllib.parse import urljoin
+
+from crawlee.crawlers import ParselCrawlingContext
+from crawlee.statistics import FinalStatistics
+
+from tulsa import Spider
+from tulsa.helpers import parse_date
+from tulsa.models import Blog
+
+
+class TrmlabsComSpider(Spider):
+    def __init__(self) -> None:
+        super().__init__(default_request_handler=self.default_request_handler)
+
+    @override
+    async def run(self) -> FinalStatistics:  # pyright: ignore [reportIncompatibleMethodOverride]
+        return await super().run(["https://www.trmlabs.com/category/insights"])
+
+    @staticmethod
+    async def default_request_handler(context: ParselCrawlingContext):
+        items = context.selector.xpath(
+            '//div[@role="listitem" and @class="list-item w-dyn-item"]'
+        )
+
+        if len(items) == 0:
+            context.log.error(f"{context.request.url} | Cannot find HTML element")
+            return
+
+        for entry in items[:20]:
+            title = entry.xpath(".//h4/text()").get()
+            if not title:
+                context.log.error(
+                    f"{context.request.url} | Cannot find title HTML element"
+                )
+                continue
+            url = entry.xpath("./div/a/@href").get()
+            if not url:
+                context.log.error(
+                    f"{context.request.url} | Cannot find url HTML element"
+                )
+                continue
+            url = urljoin(context.request.loaded_url or context.request.url, url)
+            published = entry.xpath('.//div[@class="subtitle"]/text()').get()
+            thumbnail = entry.xpath(".//img/@src").get()
+
+            item = Blog(url=url, title=title)
+            item.thumbnail = thumbnail
+            if published:
+                published = parse_date(published)
+                if published:
+                    item.published = datetime.fromtimestamp(mktime(published))
+
+            yield item
