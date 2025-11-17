97a06f5caa780ad15c3673c937647e175209deb7
zhuren-plbm <zhuren@phonglambaomat.online>
2025-11-17 10:46:31 +0700

    chore(spider): add qualys.com

---
+++ tulsa/spiders/blog/qualys_com.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from time import mktime
+from typing import override
+
+from crawlee.crawlers import ParselCrawlingContext
+from crawlee.statistics import FinalStatistics
+
+from tulsa import Spider
+from tulsa.helpers import parse_date
+from tulsa.models import Blog
+
+
+class QualysComSpider(Spider):
+    def __init__(self) -> None:
+        super().__init__(default_request_handler=self.default_request_handler)
+
+    @override
+    async def run(self) -> FinalStatistics:  # pyright: ignore [reportIncompatibleMethodOverride]
+        return await super().run(
+            ["https://www.qualys.com/research/security-advisories/"]
+        )
+
+    @staticmethod
+    async def default_request_handler(context: ParselCrawlingContext):
+        items = context.selector.xpath('//ul[@class="advisories"]/li')
+
+        if len(items) == 0:
+            context.log.error(f"{context.request.url} | Cannot find HTML element")
+            return
+
+        for entry in items:
+            title = entry.xpath(".//h4/text()").get()
+            if not title:
+                context.log.error(
+                    f"{context.request.url} | Cannot find title HTML element"
+                )
+                continue
+            url = entry.xpath(".//a/@href").get()
+            if not url:
+                context.log.error(
+                    f"{context.request.url} | Cannot find url HTML element"
+                )
+                continue
+            published = entry.xpath("./div/text()").get()
+
+            item = Blog(url=url, title=title)
+            if published:
+                published = parse_date(published)
+                if published:
+                    item.published = datetime.fromtimestamp(mktime(published))
+
+            yield item
