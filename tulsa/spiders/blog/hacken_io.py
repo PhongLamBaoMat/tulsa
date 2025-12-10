cdf33ee5cbd0ebe49ceb2579a8cea3304bdea2e7
zhuren-plbm <zhuren@phonglambaomat.online>
2025-12-10 10:21:18 +0700

    chore(spider): remove fetch_articles for hacken.io (#55)

--- tulsa/spiders/blog/hacken_io.py
+++ tulsa/spiders/blog/hacken_io.py
@@ -1,25 +1,26 @@
 import json
+from datetime import datetime
+from time import mktime
 from typing import override
-from urllib.parse import urljoin
 
 from crawlee import Request
 from crawlee.crawlers import ParselCrawlingContext
 from crawlee.statistics import FinalStatistics
+from tulsa.helpers import parse_date
+from tulsa.models import Blog, Category
 
 from tulsa import Spider
-from tulsa.models import Blog, Category
 
 
 class HackenIoSpider(Spider):
     def __init__(self) -> None:
         super().__init__(default_request_handler=self.default_request_handler)
-        self.router._handlers_by_label["fetch_articles"] = self.fetch_articles  # pyright: ignore [reportPrivateUsage]
 
     @override
     async def run(self) -> FinalStatistics:  # pyright: ignore [reportIncompatibleMethodOverride]
         return await super().run(
             [
-                Request.from_url(url, label="fetch_articles")
+                Request.from_url(url, headers={"Rsc": "1"})
                 for url in [
                     "https://hacken.io/category/case-studies/",
                     "https://hacken.io/category/reports/",
@@ -28,44 +29,28 @@ class HackenIoSpider(Spider):
         )
 
     @staticmethod
-    async def fetch_articles(context: ParselCrawlingContext):
-        items = (
-            context.selector.xpath('//li[@data-sentry-component="BlogArticleCard"]')
-            .xpath(".//a/@href")
-            .getall()
-        )
-
-        if len(items) == 0:
-            context.log.error(f"{context.request.url} | Cannot find HTML element")
-            return
-
-        await context.add_requests(
-            [
-                urljoin(context.request.loaded_url or context.request.url, url)
-                for url in items
-            ]
-        )
-
-    @staticmethod
     async def default_request_handler(context: ParselCrawlingContext):
-        data = context.selector.xpath(
-            '/html/head/meta[@name="application/ld+json"]/@content'
-        ).get()
-        if not data:
-            context.log.error(f"{context.request.url} | Cannot find json schema")
-            return
+        res = (await context.http_response.read()).decode()
+        pos = res.rfind("2f:[")
+        json_data = res[pos + 3 :].split("\n")[0]
+        j = json.loads(json_data)
+        items = j[1][3]["posts"]["list"]
 
-        item = None
-        for entry in json.loads(data)["@graph"]:
-            if entry["@type"] == "Article":
-                item = Blog.from_json_schema(entry)
-                break
+        for entry in items:
+            title = entry["title"]
+            if "/case-studies/" in context.request.url:
+                url = f"https://hacken.io/case-studies/{entry['slug']}"
+            else:
+                url = f"https://hacken.io/insights/{entry['slug']}"
+            description = entry["excerpt"]
+            thumbnail = entry["feature_image"]
+            published = parse_date(entry["published_at"])
 
-        if not item:
-            context.log.error(
-                f"{context.request.url} | Cannot find title or url element"
-            )
-            return
-        item.category = Category.Blockchain
+            item = Blog(url=url, title=title)
+            item.description = description
+            item.thumbnail = thumbnail
+            if published:
+                item.published = datetime.fromtimestamp(mktime(published))
+            item.category = Category.Blockchain
 
-        yield item
+            yield item
